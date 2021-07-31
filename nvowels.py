1cd20ff19a1c4bd3adbc569cedcb2968638f3031
soumya13042000 <soumyashreebaral086@gmail.com>
2021-07-31 11:53:55 +0800

    just added one  comment line

--- nvowels.py
+++ nvowels.py
@@ -2,7 +2,7 @@ s=input('Enter your string')
 nonvowels=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z','B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','E','X','Y','Z']
 vowels=['a','e','i','o','u','A','E','I','O','U']
 for i in s:
-    if i in vowels:  
+    if i in vowels:  # if i not in nonvowels:
         continue
     print(i)
     
