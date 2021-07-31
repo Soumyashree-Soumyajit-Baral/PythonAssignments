171c319c24b1cbd67db2c99b17b5d3d4efe9bbae
soumya13042000 <soumyashreebaral086@gmail.com>
2021-07-31 12:01:29 +0800

    added one more assignment.

---
+++ strc.py
@@ -0,0 +1,6 @@
+s=input('Enter your string number')
+print('the integer value is :',int(s))
+print('the float value is :',float(s))
+print('the boolean value is :',bool(s))
+print('the complex value is :',complex(s))
+
