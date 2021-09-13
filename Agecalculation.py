0b383dc4033ac845f5feeafd5ffdb49b91e818ec
soumya13042000 <soumyashreebaral086@gmail.com>
2021-09-13 15:16:32 +0800

    just update this assignment

--- Agecalculation.py
+++ Agecalculation.py
@@ -1,42 +1,24 @@
 from datetime import date
 
-yy=int(input('Enter your Birth Year :'))
+dd=int(input('Enter your Birth Date :'))
 mm=int(input('Enter your Birth Month :'))
-dd=int(input('Enter your Birth Date :'))        
+yy=int(input('Enter your Birth Year :'))        
 dob=date(yy,mm,dd)
 cd=date.today()
-bd=date(yy,2,28)
-l=[]
-if dob>bd:
-    for i in range(dob.year+1,cd.year):
-        if not i%4:
-            l.append(i)
+normalmonths=[31,28,31,30,31,30,31,31,30,31,30,31]
+leapmonths=[31,29,31,30,31,30,31,31,30,31,30,31]
+isleap=not dob.year%4
+if isleap:
+    months= leapmonths[mm-2]
 else:
-    for i in range(dob.year,cd.year):
-        if not i%4:
-            l.append(i)
-d1=(cd-dob).days
-y=int(d1/365)
-m=int((d1%365)/30)
-d=int(((d1%365)%30.416666666666668)-len(l))
+    months= normalmonths[mm-2]
+y=cd.year-dob.year-((cd.month,cd.day) < (dob.month,dob.day))
+m=(((cd.month-1)+12)-dob.month if(dob.day>cd.day and dob.month>=cd.month)else(cd.month-1)-dob.month if((cd.month>dob.month)and(dob.day>cd.day))
+   else(cd.month+12)-dob.month if ((cd.month<dob.month) and (dob.day<cd.day)) else (cd.month-dob.month))
+d=((cd.day+months)-dob.day if (dob.day>cd.day and dob.month>=cd.month)else (cd.day+months)-dob.day if((cd.month>dob.month)and(dob.day>cd.day))
+   else cd.day-dob.day if ((cd.month<dob.month) and (dob.day<cd.day)) else cd.day-dob.day)
 print(f'Your Age is : {y}years, {m}months, {d}days')
 print('Thank You ! Please Come again whenever you forget your Age.')
 
-'''
-#yy=int(input('Enter your Birth Year :'))
-#mm=int(input('Enter your Birth Month :'))
-#dd=int(input('Enter your Birth Date :'))        
-#dob=date(yy,mm,dd)
-dob=date(2000,4,13)
-cd=date.today()
-d1=(cd-dob)
-print(d1)
-d2=d1.days
-y=int(d2/365)
-m=int((d2%365)/30)
-d=int((d2%365)%30.4167)
-print(f'Your Age is : {y}years, {m}months, {d}days')
-print('Thank You ! Please Come again whenever you forget your Age.')
 
-'''
 
